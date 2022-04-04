34092de1b2fd66a8c648b7abde95a6115a31eb9e
xiexuan-star <xiexuan127@foxmail.com>
2022-04-05 05:28:11 +0800

    feat: simple diff complete

--- example/src/main.ts
+++ example/src/main.ts
@@ -3,75 +3,29 @@ import { COMMENT_NODE, FRAGMENT_NODE, renderer, TEXT_NODE, VNode } from '../../r
 
 const { render } = renderer;
 const currentVal = ref('init');
-const listLen = ref(0);
+const toggleList = ref(false);
 const alert = ref(false);
 const onBtnClick = (e: any) => {
   alert.value = true;
 };
+const oldNodes = [
+  { type: 'h1', key: 0, children: 'h1' },
+  { type: 'h2', key: 1, children: 'h2' },
+  { type: 'h3', key: 2, children: 'h3' },
+];
+const newNodes = [
+  { type: 'h1', key: 0, children: 'h11' },
+  { type: 'h3', key: 2, children: 'h33' },
+  { type: 'h2', key: 1, children: 'h22' },
+  { type: 'h4', key: 3, children: 'h44' }
+];
 effect(() => {
   const nodes: VNode = {
-    type: 'h1', props: {
-      id: 'foo',
-      style: {
-        display: 'flex',
-        'justify-content': 'center',
-        flexDirection: 'column',
-        width: '200px'
-      },
-      onClick: alert.value ? (e: any) => {
-        console.log(e);
-      } : null
-    }, children: [
-      { type: 'article', children: currentVal.value },
-      { type: 'nav', children: 'this is a nav' },
-      {
-        type: 'input',
-        props: { value: 'bar', disabled: '' }
-      },
-      {
-        type: 'button', props: {
-          onClick: onBtnClick
-        },
-        children: 'clickme'
-      },
-      { type: TEXT_NODE, children: 'text' },
-      { type: COMMENT_NODE, children: 'comment' },
-      { type: 'br' },
-      {
-        type: FRAGMENT_NODE, children: [
-          { type: COMMENT_NODE, children: 'fragment comment' },
-          { type: TEXT_NODE, children: 'fragment' },
-        ]
-      },
-      {
-        type: 'input',
-        props: {
-          value: currentVal.value,
-          onInput(e: any) {
-            currentVal.value = e.target.value;
-            console.log(e.target.value);
-          },
-          class: ['class1 class2', { class3: true, class4: false }]
-        }
-      },
-      {
-        type: 'button', props: {
-          onClick: () => {
-            listLen.value++;
-          }
-        },
-        children: 'add'
-      },
-      {
-        type: 'ul',
-        children: Array.from({ length: listLen.value }).map((_, i) => {
-          return {
-            type: 'li',
-            children: `item ${ i }`
-          };
-        })
-      }
-    ]
+    type: 'ul',
+    children: toggleList.value ? newNodes : oldNodes
   };
   render(nodes, '#app');
 });
+setInterval(() => {
+  toggleList.value = !toggleList.value;
+}, 2_000);
--- renderer/index.ts
+++ renderer/index.ts
@@ -1,3 +1,4 @@
+import { last } from 'lodash-es';
 import { isArray, isFunction, isObject, isString } from '../shared';
 
 export interface VNode {
@@ -29,7 +30,7 @@ interface RendererOptions {
 
   patchProps(el: any, prop: string, preVal: any, nextVal: any): void;
 
-  insert(el: any, parent: any, anchor?: any | null): void;
+  insert(el: any, parent: any, anchor?: any): void;
 }
 
 interface Container extends HTMLElement {
@@ -62,7 +63,7 @@ function createRenderer(options: RendererOptions) {
     container._vnode = vnode;
   }
 
-  function patch(n1: VNode | null, n2: VNode, container: any) {
+  function patch(n1: VNode | null, n2: VNode, container: any, anchor?: any) {
     if (n1 && n1.type !== n2.type) {
       unmount(n1);
       n1 = null;
@@ -77,7 +78,7 @@ function createRenderer(options: RendererOptions) {
     } else if (type === TEXT_NODE) {
       if (!n1) {
         const el = n2.el = createTextNode(n2.children as string);
-        insert(el, container, null);
+        insert(el, container, anchor);
       } else {
         const el = n2.el = n1.el as Text;
         if (n2.children !== n1.children) {
@@ -87,7 +88,7 @@ function createRenderer(options: RendererOptions) {
     } else if (type === COMMENT_NODE) {
       if (!n1) {
         const el = n2.el = createCommentNode(n2.children as string);
-        insert(el, container, null);
+        insert(el, container, anchor);
       } else {
         const el = n2.el = n1.el as Comment;
         if (n2.children !== n1.children) {
@@ -99,7 +100,7 @@ function createRenderer(options: RendererOptions) {
         if (isArray(n2.children)) {
           // 为什么不使用HTMLFragment?
           n2.children?.forEach(node => {
-            patch(null, node, container);
+            patch(null, node, container, anchor);
           });
         }
       } else {
@@ -154,17 +155,43 @@ function createRenderer(options: RendererOptions) {
     const newChildren = n2.children as Array<VNode>;
     const oldLen = oldChildren.length;
     const newLen = newChildren.length;
-    const commonLen = Math.min(oldLen, newLen);
-    for (let i = 0; i < commonLen; i++) {
-      patch(oldChildren[i], newChildren[i], container);
-    }
-    if (oldLen > commonLen) {
-      for (let i = commonLen; i < oldLen; i++) {
-        unmount(oldChildren[i]);
+    // 当前递增序列中最大的index
+    let lastIndex = 0;
+    for (let i = 0; i < newLen; i++) {
+      const newNode = newChildren[i];
+      let find = false;
+      for (let j = 0; j < oldLen; j++) {
+        const oldNode = oldChildren[j];
+        if (oldNode.key === newNode.key) {
+          find = true;
+          patch(oldNode, newNode, container);
+          // 如果节点在旧列表中的索引不满足递增,就说明需要移动
+          if (j < lastIndex) {
+            const preNode = newChildren[i - 1];
+            if (preNode) {
+              const anchor = preNode.el?.nextSibling;
+              // 移动到已经排序好的上一个新节点的后面
+              insert(oldNode.el, container, anchor);
+            }
+          } else {
+            lastIndex = j;
+          }
+          break;
+        }
       }
-    } else if (newLen > commonLen) {
-      for (let i = commonLen; i < newLen; i++) {
-        patch(null, newChildren[i], container);
+      // 如果在旧节点中没找到，说明是新增
+      if (!find) {
+        const preNode = newChildren[i - 1];
+        let anchor = preNode ? preNode.el!.nextSibling : container.firstChild;
+        patch(null, newNode, container, anchor);
+      }
+    }
+    // 同时，如果旧节点在新节点中没找到，说明是移除
+    for (let i = 0; i < oldLen; i++) {
+      const oldNode = oldChildren[i];
+      const has = newChildren.find(newNode => newNode.key === oldNode.key);
+      if (!has) {
+        unmount(oldNode);
       }
     }
   }
@@ -300,7 +327,7 @@ export const renderer = createRenderer({
   unmount,
   insert(el: HTMLElement, parent: HTMLElement, anchor?: HTMLElement) {
     if (anchor) {
-      el.insertBefore(parent, anchor);
+      parent.insertBefore(el, anchor);
     } else {
       parent.appendChild(el);
     }
