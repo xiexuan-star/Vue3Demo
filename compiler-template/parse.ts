461489a2b620086145c6ba95852f980b6ed26534
xiexuan-star <xiexuan127@foxmail.com>
2022-04-14 23:48:25 +0800

    chore: some function init

--- compiler-template/parse.ts
+++ compiler-template/parse.ts
@@ -1,8 +1,9 @@
 /**
  * DATA initial mode
- * CDATA
- * RCDATA
- * RAWTEXT
+ * CDATA any symbol will be used as a char
+ * RCDATA textarea and title tag
+ * RAWTEXT style xmp iframe noembed noframes noscript .etc tag
+ *         not support html entity
  *
  * HTML实体
  * */
@@ -16,9 +17,17 @@ enum TextNodes {
 
 interface Context {
   source: string;
-
+  mode: TextNodes;
 }
 
 function parse(str: string) {
-  const context: Context = { source: str };
+  // 递归下降算法
+  const context: Context = { source: str, mode: TextNodes.DATA };
+  const nodes = parseChildren(context, []);
+  return { type: 'Root', children: nodes };
+}
+
+function parseChildren(context: Context, stack: Array<any>) {
+  //
+  return [];
 }
